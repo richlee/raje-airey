9f7d3b8e0d1379285ab82907a39b027be5f1a826
Rich Lee <richalee@pm.me>
2026-01-18 19:08:01 +0000

    Add git-gateway settings endpoint

---
+++ functions/.netlify/git/settings.js
@@ -0,0 +1,26 @@
+// Git Gateway settings endpoint
+// Returns configuration for the CMS
+
+export async function onRequestGet(context) {
+  return new Response(JSON.stringify({
+    github_enabled: true,
+    gitlab_enabled: false,
+    bitbucket_enabled: false,
+    roles: null
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
+}
